2c08381f6effb189868a9153d2eef8b3921e9ee7
pvllsv <pavellisov69@gmail.com>
2025-11-25 15:05:10 +0300

    first commit

---
+++ js/scriprt.js
@@ -0,0 +1,33 @@
+$(".js-tabs-link").aniTabs({
+  animation:"slide", 
+  slideDirection:"right" // or right
+});
+
+$('.taxanomy__items').slick({
+  infinite: true,
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  centerMode: true,
+  centerPadding: '0',
+  // autoplay:true,
+  prevArrow:'<button type = "button" class = "slick-prev">  </ button>',
+  nextArrow:'<button type = "button" class = "slick-next">  </ button>',
+  responsive: [
+	    {
+	      breakpoint: 850,
+	      settings: {
+	        slidesToShow: 2,
+          centerMode: false
+	      }
+	    },
+	    {
+	      breakpoint: 600,
+	      settings: {
+	        slidesToShow: 1,
+	      }
+	    }
+    ]
+});
+
+
+
